a76c9615afcfb0b6f65c22b2a5d8126011b8dcaa
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-05-31 15:38:12 -0400

    Main Axios call working based on username and repo, working on the contributors .get and then the license .get

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,108 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require('axios');
+// const markdown =  require('./utils/generateMarkdown');
+
+
 const questions = [
+    {
+    type: "input",
+    name: "repo",
+    message: "What is your Repo title?"
+}, 
+{
+    type: "input",
+    name: "username",
+    message: "What is your GitHub Username?"
+}];
 
-];
+inquirer.prompt(questions)
+    .then(function( {username, repo} ) {
+        const queryUrl = `https://api.github.com/repos/${username}/${repo}`;
+        const contribUrl = `https://api.github.com/repos/${username}/${repo}/contributors`;
+        axios
+        
+        .get(queryUrl)
+        .then(res =>{
+            if (res.data.description != null){
+                let description = res.data.description;
+            }else{
+                let description = "Description Needed";
+            }
+            
+            let installation = "Enter installation instructions."; 
+            console.log(installation)
+            let usage = "Enter Usage Instructions.";
+            console.log(usage)
+            let tests = "Enter any and all tests run."; 
+            console.log(tests)
+            let questions = "Using the email link below, please reach out with any questions.";
+            console.log(questions)
+            axios
+            .get(contribUrl)
+            .then(data => {
+                const contributors = [];
+                data.data.forEach(element => {
+                    contributors.push(element.login);
+                    console.log(contributors);
+                })
+                
+            });
+            let license = res.data.license;
+            console.log(license)
 
-function writeToFile(fileName, data) {
-}
+            // // const writeReadMe = ()=>{
+            // //     if(description != undefined && installation != undefined && usage != undefined && faq != undefined && license !== undefined)
+            // //         {
+            // //             const readme=
+            // //         `
+            // //             # ${repo}
+            // //             #### Author: ${username}
 
-function init() {
+            // //             ${description}
 
-}
+            // //             //badge 
 
-init();
+            // //             # Table of Contents
+            // //             * ##Installation
+            // //             * ##Usage
+            // //             * ##Tests
+            // //             * ##Questions
+            // //             * ##Contributors
+            // //             * ##License
+
+            // //             ##Installation
+            // //             ${installation}
+
+            // //             ##Usage
+            // //             ${usage}
+
+            // //             ##Tests
+            // //             ${tests}
+
+            // //             ##Questions
+            // //             ${questions}
+
+            // //             ##Contributors
+            // //             ${contributors}
+
+            // //             ##License
+            // //             ${license}
+            // //             `
+            // //         };
+            // //     fs.writeFile(`${repo} README.md`, readme, function(err){
+            // //         if (err) console.log(err)
+            // //     })
+            // }     
+        })
+})
+
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
